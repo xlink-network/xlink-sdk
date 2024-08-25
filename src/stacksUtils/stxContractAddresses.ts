d7b6a12ea26c198265ec7a3a055011469fc2021f
c4605 <bolasblack@gmail.com>
2024-08-25 13:01:32 +0100

    refactor: update stacks contract addresses

--- src/stacksUtils/stxContractAddresses.ts
+++ src/stacksUtils/stxContractAddresses.ts
@@ -1,19 +1,19 @@
-import {
-  STACKS_CONTRACT_DEPLOYER_MAINNET,
-  STACKS_CONTRACT_DEPLOYER_TESTNET,
-} from "../config"
 import { KnownChainId, KnownTokenId } from "../utils/types/knownIds"
 import { StacksContractAddress } from "../xlinkSdkUtils/types"
 
-const xlinkContractsDeployerMainnet = STACKS_CONTRACT_DEPLOYER_MAINNET
-const xlinkContractsDeployerTestnet = STACKS_CONTRACT_DEPLOYER_TESTNET
+export const xlinkContractsDeployerMainnet =
+  "SP2XD7417HGPRTREMKF748VNEQPDRR0RMANB7X1NK"
+export const xlinkContractsDeployerTestnet =
+  "ST2QXSK64YQX3CQPC530K79XWQ98XFAM9W3XKEH3N"
 
-const alexContractDeployerMainnet = "SP102V8P0F7JX67ARQ77WEA3D3CFB5XW39REDT0AM"
-const alexContractDeployerTestnet = "ST1J4G6RR643BCG8G8SR6M2D9Z9KXT2NJDRK3FBTK"
+export const alexContractDeployerMainnet =
+  "SP102V8P0F7JX67ARQ77WEA3D3CFB5XW39REDT0AM"
+export const alexContractDeployerTestnet =
+  "ST1J4G6RR643BCG8G8SR6M2D9Z9KXT2NJDRK3FBTK"
 
-const legacyAlexContractDeployerMainnet =
+export const legacyAlexContractDeployerMainnet =
   "SP3K8BC0PPEVCV7NZ6QSRWPQ2JE9E5B6N3PA0KBR9"
-const legacyAlexContractDeployerTestnet =
+export const legacyAlexContractDeployerTestnet =
   "ST1J2JTYXGRMZYNKE40GM87ZCACSPSSEEQVSNB7DC"
 
 export const stxTokenContractAddresses: Record<
@@ -72,21 +72,21 @@ export const stxTokenContractAddresses: Record<
   },
   [KnownTokenId.Stacks.vLiSTX]: {
     [KnownChainId.Stacks.Mainnet]: {
-      deployerAddress: legacyAlexContractDeployerMainnet,
+      deployerAddress: alexContractDeployerMainnet,
       contractName: "token-wvlqstx",
     },
     [KnownChainId.Stacks.Testnet]: {
-      deployerAddress: legacyAlexContractDeployerTestnet,
+      deployerAddress: alexContractDeployerTestnet,
       contractName: "token-wvlqstx",
     },
   },
   [KnownTokenId.Stacks.vLiALEX]: {
     [KnownChainId.Stacks.Mainnet]: {
-      deployerAddress: xlinkContractsDeployerMainnet,
+      deployerAddress: alexContractDeployerMainnet,
       contractName: "token-wvlialex",
     },
     [KnownChainId.Stacks.Testnet]: {
-      deployerAddress: xlinkContractsDeployerTestnet,
+      deployerAddress: alexContractDeployerTestnet,
       contractName: "token-wvlialex",
     },
   },
