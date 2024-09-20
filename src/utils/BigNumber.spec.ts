c70278defae5e6184c3e25d4f04eb5a15031358d
c4605 <bolasblack@gmail.com>
2024-09-20 17:02:17 +0200

    test: fix unit test

--- src/utils/BigNumber.spec.ts
+++ src/utils/BigNumber.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest"
-import { BigNumber, BigNumberSource } from "../BigNumber"
+import { BigNumber, BigNumberSource } from "./BigNumber"
 
 describe("BigNumber", () => {
   describe("isEven", () => {
