ee6e63b0da9c56284f034d472fcc3acf050e34b7
c4605 <bolasblack@gmail.com>
2024-10-05 19:30:19 +0100

    feat: add new export /lowlevelUnstableInfos

---
+++ src/lowlevelUnstableInfos.ts
@@ -0,0 +1,9 @@
+/**
+ * @experimental these APIs are not stable and may change in the future, and are
+ *   not covered by semantic versioning. Please use them at your own risk.
+ */
+
+export {
+  contractAssignedChainIdFromKnownChain,
+  contractAssignedChainIdToKnownChain,
+} from "./stacksUtils/crossContractDataMapping"
