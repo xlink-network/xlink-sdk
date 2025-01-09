3906740041ddf8301bd0e0c250a57fc889d1d706
c4605 <bolasblack@gmail.com>
2025-01-09 16:34:18 +0100

    fixup! fix: stacks contract name

--- src/config.ts
+++ src/config.ts
@@ -21,19 +21,20 @@ export const contractNameOverrides_testnet: Record<string, string> = {
 
   // btc bridge
   "btc-peg-in-endpoint-v2-05": "btc-peg-in-endpoint-v2-05-dc",
+  "btc-peg-in-endpoint-v2-07-swap": "btc-peg-in-v2-07-swap-01",
   "btc-peg-in-endpoint-v2-05-lisa": "btc-peg-in-endpoint-v2-05-lisa-dc",
-  "btc-peg-in-v2-07-swap": "btc-peg-in-v2-07-swap-01",
-  "btc-peg-in-v2-05-launchpad": "btc-peg-in-v2-05-launchpad-dc",
+  "btc-peg-in-endpoint-v2-05-launchpad": "btc-peg-in-v2-05-launchpad-dc",
   "btc-peg-out-endpoint-v2-01": "btc-peg-out-endpoint-v2-05-dc",
   // meta bridge
   "meta-peg-in-endpoint-v2-04": "meta-peg-in-endpoint-v2-04-dc",
+  "meta-peg-in-endpoint-v2-06-swap": "meta-peg-in-v2-06-swap-01",
   "meta-peg-in-endpoint-v2-04-lisa": "meta-peg-in-endpoint-v2-04-lisa-dc",
-  "meta-peg-in-v2-06-swap": "meta-peg-in-v2-06-swap-01",
   "meta-peg-out-endpoint-v2-04": "meta-peg-out-endpoint-v2-04-dc",
   // cross bridge
-  "cross-peg-in-v2-04-swap": "cross-peg-in-v2-04-swap-da",
-  "cross-peg-in-v2-04-launchpad": "cross-peg-in-endpoint-v2-04-launchpad-dc",
   "cross-peg-in-endpoint-v2-04": "cross-peg-in-endpoint-v2-04-dc",
+  "cross-peg-in-endpoint-v2-04-swap": "cross-peg-in-v2-04-swap-da",
+  "cross-peg-in-endpoint-v2-04-launchpad":
+    "cross-peg-in-endpoint-v2-04-launchpad-dc",
   // cross router
   "cross-router-v2-03": "cross-router-v2-03-dc",
 }
