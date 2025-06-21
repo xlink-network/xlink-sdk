fbda1f3293d63fcf94f2c898ba291a1e113210a2
c4605 <bolasblack@gmail.com>
2025-06-21 11:51:42 +0200

    fixup! feat: add instant swap support

--- src/metaUtils/swapHelpers.ts
+++ src/metaUtils/swapHelpers.ts
@@ -1,3 +1,4 @@
+import { SDKGlobalContext } from "../sdkUtils/types.internal"
 import { BigNumber } from "../utils/BigNumber"
 import { KnownRoute_FromMeta } from "../utils/buildSupportedRoutes"
 import {
@@ -8,9 +9,12 @@ import {
   EVMDexAggregatorSwapParameters,
   getPossibleEVMDexAggregatorSwapParametersImpl,
 } from "../utils/swapHelpers/evmDexAggregatorSwapParametersHelpers"
+import {
+  getInstantSwapParametersImpl,
+  InstantSwapParameters,
+} from "../utils/swapHelpers/instantSwapParametersHelpers copy"
 import { KnownChainId, KnownTokenId } from "../utils/types/knownIds"
-import { SDKGlobalContext } from "../sdkUtils/types.internal"
-import { getMeta2StacksFeeInfo } from "./peggingHelpers"
+import { getInstantSwapFeeInfo, getMeta2StacksFeeInfo } from "./peggingHelpers"
 
 export async function getALEXSwapParameters_FromMeta(
   sdkContext: SDKGlobalContext,
@@ -54,4 +58,51 @@ export async function getPossibleEVMDexAggregatorSwapParameters_FromMeta(
         { swapRoute: { via: "evmDexAggregator" } },
       ),
   })
+}
+
+export async function getInstantSwapParameters_FromMeta(
+  sdkContext: SDKGlobalContext,
+  info: KnownRoute_FromMeta & {
+    amount: BigNumber
+  },
+): Promise<undefined | InstantSwapParameters> {
+  const { fromChain, fromToken, toChain, toToken } = info
+
+  if (
+    KnownChainId.isRunesChain(fromChain) &&
+    KnownTokenId.isRunesToken(fromToken) &&
+    KnownChainId.isBitcoinChain(toChain) &&
+    KnownTokenId.isBitcoinToken(toToken)
+  ) {
+    return getInstantSwapParametersImpl(sdkContext, {
+      ...info,
+      getInstantSwapTransferProphet: () =>
+        getInstantSwapFeeInfo(sdkContext, {
+          fromChain,
+          fromToken,
+          toChain,
+          toToken,
+        }),
+    })
+  }
+
+  if (
+    KnownChainId.isRunesChain(fromChain) &&
+    KnownTokenId.isRunesToken(fromToken) &&
+    KnownChainId.isRunesChain(toChain) &&
+    KnownTokenId.isRunesToken(toToken)
+  ) {
+    return getInstantSwapParametersImpl(sdkContext, {
+      ...info,
+      getInstantSwapTransferProphet: () =>
+        getInstantSwapFeeInfo(sdkContext, {
+          fromChain,
+          fromToken,
+          toChain,
+          toToken,
+        }),
+    })
+  }
+
+  return undefined
 }
